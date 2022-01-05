636b231a7f7b21f7181aa9396db0340ff8e5f2f0
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-05 22:46:41 +0800

    Delete iOS directory

--- iOS/Manager/EZShopManager/EZShopManager/User.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class User {
-	var name = ""
-	var photo = ""
-	var user_id = ""
-	var isInStore = false
-	var items: [Item] = []
-}
--- iOS/User/ezshopUser/Pods/Target Support Files/Pods-ezshopUser/Pods-ezshopUser-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_ezshopUserVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ezshopUserVersionString[];
-
